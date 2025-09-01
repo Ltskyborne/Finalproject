22798b001247d3abad14878546031ab64e0dd790
Ltskyborne <dominicevanderdaniel@gmail.com>
2025-09-01 14:46:04 +0700

    awalan

---
+++ footer.php
@@ -0,0 +1,6 @@
+  </main>
+  <footer>
+    <p>&copy; <?php echo date("Y"); ?> Dominic’s Portfolio</p>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include "header.php"; ?>
+  <section class="intro">
+    <h1>Hello Everyone, I'm Dominic</h1>
+    <p>I like to swim. I’m now in Junior High School, currently in 8th grade at <strong>SMPK BPK Penabur</strong>.  
+    I’m also a passionate <strong>F1 fan</strong>!</p>
+  </section>
+<?php include "footer.php"; ?>
